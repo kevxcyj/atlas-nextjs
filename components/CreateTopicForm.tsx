583266c118bf50070f24430d4e4aa4acf27761fb
kevxcyj <kevcyjunior@gmail.com>
2025-10-01 19:19:21 -0500

    Pushed current build

--- components/CreateTopicForm.tsx
+++ components/CreateTopicForm.tsx
@@ -1,6 +1,9 @@
+import { addTopic } from "@/lib/actions";
+import Link from "next/link";
+
 export default function CreateTopicForm() {
   return (
-    <form>
+    <form action={addTopic}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         <div className="mb-4">
           <label htmlFor="title" className="mb-2 block text-sm font-medium">
@@ -20,12 +23,12 @@ export default function CreateTopicForm() {
         </div>
       </div>
       <div className="mt-6 flex justify-end gap-4">
-        <a
+        <Link
           href="/ui"
           className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
         >
           Cancel
-        </a>
+        </Link>
         <button type="submit">Create Topic</button>
       </div>
     </form>
